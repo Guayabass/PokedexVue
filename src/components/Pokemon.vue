b129f2bae7b95b4760f82a82414b9f8836b85870
Guayabas <guayabas0399@gmail.com>
2022-08-23 22:18:20 -0600

    reset button and finished logic for changing pics

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,10 +1,19 @@
 <template>
     <section class="Pokemon" v-if="Object.entries(checkPokemon()).length > 0">
         {{ capitalize(checkPokemon().name) }}
-        <img v-if="!shiny" v-bind:src="loadImage()" :alt="checkPokemon().name" />
-        <img v-else :src="loadShinyImage()" :alt="checkPokemon().name" />
-        <button @click="changeImage()">shiny</button>
+        <img class="pokemons" v-if="!shiny && !gif && static" v-bind:src="loadImage()" :alt="checkPokemon().name" />
+        <img class="pokemons" v-else-if="shiny && !gif && static" v-bind:src="loadShinyImage()" :alt="checkPokemon().name" />
+        <img class="pokemons" v-else-if="!shiny && gif && !static" v-bind:src="loadGif()" :alt="checkPokemon().name" />
+        <img class="pokemons" v-else-if="shiny && gif && !static" v-bind:src="loadShinyGif()" :alt="checkPokemon().name" />
+        <button @click="shiny = true">Shiny</button>
         <button @click="loadCry()">Cry</button>
+        <button @click="static = false, gif = true">Gif</button>
+        <button @click="static = true, gif = false, shiny = false">Reset</button>
+        <!---<img v-else-if="!shiny && gif" v-bind:src="loadGif()" :alt="checkPokemon().name" />
+        <img v-else :src="loadShinyImage()" :alt="checkPokemon().name" />
+        <button @click="changeImage()">Shiny</button>
+        <button @click="loadCry()">Cry</button>
+        <button @click="changeGif()">Gif</button>-->
     </section>
 </template>
 
@@ -18,40 +27,49 @@ export default {
     data() {
         return {
             shiny: false,
+            gif: false,
+            static: true,
         }
     },
     methods: {
         checkPokemon() {
             const pokemonStore = usePokemonStore()
-            this.img = 'src/assets/pokemon/' + pokemonStore.pokemonID + '.png'
             return pokemonStore.pokemonData
         },
         loadImage() {
             const pokemonStore = usePokemonStore()
-            //console.log(this.hover)
             return 'src/assets/pokemon/' + pokemonStore.pokemonID + '.png'
-            
+            //return 'https://img.pokemondb.net/sprites/black-white/anim/normal/' + pokemonStore.pokemonData.name + '.gif'
         },
-        changeImage(){
-            if (this.shiny ) {
-                this.shiny = false
-            } else {
-                this.shiny = true
-            }
-        },
-        loadShinyImage() {
-            const pokemonStore = usePokemonStore()
-            //console.log(this.hover)
-            return 'src/assets/pokemon/shiny/' + pokemonStore.pokemonID + '.png'
-        },
-        loadCry(){
+        loadCry() {
             const pokemonStore = usePokemonStore()
             const audio = new Audio('src/assets/cries/' + pokemonStore.pokemonID + '.ogg');
             audio.play();
         },
-        capitalize(string){
-           return string.charAt(0).toUpperCase() + string.slice(1); 
+        capitalize(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        },
+        loadShinyImage() {
+            const pokemonStore = usePokemonStore()
+            return 'src/assets/pokemon/shiny/' + pokemonStore.pokemonID + '.png'
+        },
+        loadGif() {
+            const pokemonStore = usePokemonStore()
+            return 'https://img.pokemondb.net/sprites/black-white/anim/normal/' + pokemonStore.pokemonData.name + '.gif'
+        },
+        loadShinyGif(){
+            const pokemonStore = usePokemonStore()
+            return 'https://img.pokemondb.net/sprites/black-white/anim/shiny/' + pokemonStore.pokemonData.name + '.gif' 
         }
     }
 }
 </script>
+
+<style>
+
+.pokemons{
+    width: 150px;
+    height: 150px;
+}
+
+</style>
--- src/stores/pokemonStore.js
+++ src/stores/pokemonStore.js
@@ -4,14 +4,15 @@ export const usePokemonStore = defineStore({
   id: 'Pokemons',
   state: () => ({
     pokemonData: {},
-    pokemonID: 0
+    pokemonID: 0,
   }),
   /**getters: {
     doubleCount: (state) => state.counter * 2
   },**/
   actions: {
-    increment() {
-      console.log('www')
+    changeImage() {
+      //pokemonStore.pokemonSprite = 'src/assets/pokemon/shiny/' + pokemonStore.pokemonID + '.png'
+      //console.log(pokemonStore.pokemonSprite)
     }
   }
 })
