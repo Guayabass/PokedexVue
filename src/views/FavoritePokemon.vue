22a9867f753089a36f80cf60693e55078a224054
Guayabas <guayabas0399@gmail.com>
2023-04-08 20:54:13 -0600

    added favorite logic when there are no favorites

--- src/views/FavoritePokemon.vue
+++ src/views/FavoritePokemon.vue
@@ -1,12 +1,15 @@
 <template>
   <NavBar></NavBar>
-  <div class="fav-container">
+  <div v-if="authStore.favorites.length > 0" class="fav-container">
     <div class="favs" v-for="(fav, index, key) in authStore.favorites" :key="key">
       <img class="fav-sprite" :src="loadSprite(fav.id)" :alt="fav" />
       <h2>{{ fav.name }}</h2>
-      <p>{{ index }}</p>
-      <p>{{ fav.id }}</p>
-      <button @click="authStore.deleteFavorite(fav.id, index)" >delete</button>
+      <button @click="authStore.deleteFavorite(fav.id, index)">delete</button>
+    </div>
+  </div>
+  <div v-else class="fav-container">
+    <div class="sub-title-wrapper">
+      <h2 class="sub-title">You currently have no <span @click="pustToPokemon()" class="blue">favorites!</span> Click<span @click="pustToPokemon()" class="blue"> here</span> to be able to lookup your favorite pokemon and <span @click="pustToPokemon()" class="blue">favorite/share</span> them!</h2>
     </div>
   </div>
 </template>
@@ -15,36 +18,39 @@
 import { useAuthStore } from '../stores/authStore';
 import axios from 'axios';
 import { localhostApi } from '../exports/nestapi.js';
+import { useRouter } from 'vue-router';
 import NavBar from '../components/NavBar.vue'
 export default {
   name: 'FavoritePokemon',
   setup() {
     const authStore = useAuthStore()
 
-    return { authStore }
+    const router = useRouter()
+
+    return { authStore, router }
   },
   async mounted() {
     if (this.authStore.isLoggedIn) {
       await axios.get(localhostApi + 'favorites/' + this.authStore.userId).then(response => { //PASS THE USERID AS PROPS INSTEAD
         response.data.forEach(element => {
-          if (this.authStore.favorites.find( e => e.id === element.pokemonID )) { 
+          if (this.authStore.favorites.find(e => e.id === element.pokemonID)) {
             console.log('Duplicate ID, not adding to array.')
           } else {
             const pokemon = {
               id: 0,
               name: "",
             }
-              pokemon.id = element.pokemonID,
-              pokemon.name = element.pokemonName, 
-            
-            this.authStore.favorites.push(pokemon);
+            pokemon.id = element.pokemonID,
+              pokemon.name = element.pokemonName,
+
+              this.authStore.favorites.push(pokemon);
           }
         });
         //console.log(this.authStore.favorites)
       })
-      .catch((error) => {
-            console.log('error in favorites')
-            console.log(error.code)
+        .catch((error) => {
+          console.log('error in favorites')
+          console.log(error.code)
         });
     } else {
       console.log('not logged (fav page)')
@@ -65,12 +71,21 @@ export default {
       }
       //return 'https://img.pokemondb.net/sprites/black-white/anim/normal/' + pokemonStore.pokemonData.name + '.gif'
     },
-    async favPokemons(){
-
+    pustToPokemon(){
+      this.router.push('/pokemon/');
     }
   },
   components: { NavBar },
 };
 </script>
 
-<style></style>
+<style>
+
+.fav-container{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
